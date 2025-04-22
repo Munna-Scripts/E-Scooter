85cb24194587dc7b94ca682f338e8020a557d8a9
Munna-Scripts <munnascripts@gmail.com>
2025-04-22 17:33:18 +0600

    Colors Section Responsive

--- js/script.js
+++ js/script.js
@@ -57,7 +57,23 @@ $('.slider-for').slick({
   slidesToScroll: 1,
   arrows: false,
   fade: true,
-  asNavFor: '.slider-nav'
+  asNavFor: '.slider-nav',
+  responsive: [,
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:true,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:1500,
+        infinite:true,
+        prevArrow:'<i class="fa-sharp arrows prr fa-solid fa-arrow-left-long"></i>',
+        nextArrow:'<i class="fa-sharp arrows nrr fa-solid fa-arrow-right-long"></i>',
+      }
+    }
+  ]
 });
 $('.slider-nav').slick({
   slidesToShow: 4,
